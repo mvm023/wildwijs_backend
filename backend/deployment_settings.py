10b2b1800ea131d8c050b457c4e73a7604ec2d11
melle <mellevanmarle@outlook.com>
2025-03-01 10:17:51 +0100

    Meer typo's

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -3,13 +3,13 @@ import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
-CSRF_TRUSTED_ORIGINS = [f"https://{os.environ.get['RENDER_EXTERNAL_HOSTNAME']}"]
+CSRF_TRUSTED_ORIGINS = [f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}"]
 
 DDEBUG = False
 
-SECRET_KEY = os.environ.get(['SECRET_KEY'])
+SECRET_KEY = os.environ.get(('SECRET_KEY'))
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -36,7 +36,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get['DATABASE_URL'],
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
     )
 }
