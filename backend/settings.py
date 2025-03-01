bd1a818c15758b2f1c9a24de3eedd56de4546e03
melle <mellevanmarle@outlook.com>
2025-03-01 10:22:34 +0100

    Werkt dit?

--- backend/settings.py
+++ backend/settings.py
@@ -18,9 +18,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "client", "build", "static"),
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media settings
