97809b4121e5eb98ea729893692a79be9815c762
melle <70153318+mvm023@users.noreply.github.com>
2025-04-28 20:15:06 +0200

    Backend changes for deploy

--- backend/settings.py
+++ backend/settings.py
@@ -142,3 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
